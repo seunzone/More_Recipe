b801a89b4a0333d35adc467975f5a282dfdd0faf
seunzone <darealseun@gmail.com>
2017-10-26 20:01:42 +0100

    created the API routes for the App

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-
+import router from './routes/routes';
 // Defining the Port Variable
 const port = process.env.PORT || 3000;
 
@@ -16,12 +16,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./routes')(app);
+router(app);
 
-
-// Default route that sends back a welcome message in JSON format.
+// Anyother route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of something.',
+  message: 'Welcome to the beginning of nothing',
 }));
 
 app.listen(port);
---
+++ server/controllers/controllerRecipe.js
@@ -0,0 +1,141 @@
+import db from '../models/jsondata';
+
+/**
+ *
+ *
+ * @class Recipe
+ */
+class Recipe {
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
+   * @returns {json} adds a recipe
+   * @memberof Recipe
+   */
+  addRecipe(req, res) {
+    const {
+      userId, name, ingredients, description
+    } = req.body;
+
+    if (!userId) {
+      return res.status(400)
+        .send('Recipe should have an owner');
+    }
+    if (!name) {
+      return res.status(400)
+        .send('Recipe should have a name');
+    }
+    if (!ingredients) {
+      return res.status(400)
+        .send('Recipe should have ingredients');
+    }
+    if (!description) {
+      return res.status(400)
+        .send('Recipe should have directions to cook');
+    }
+    if (ingredients.trim().length < 1) {
+      return res.status(400)
+        .send('Ingredients are empty');
+    }
+    if (description.trim().length < 1) {
+      return res.status(400)
+        .send('Recipe should have a direction to cook');
+    }
+    const id = db.recipes.length + 1;
+    const newRecipe = {
+      id,
+      userId,
+      name,
+      ingredients: [ingredients],
+      description,
+      downVote: 0,
+      upVote: 0
+    };
+    db.recipes.push(newRecipe);
+    return res.status(201)
+      .json({
+        status: 'success',
+        message: 'Recipe added',
+        recipe: newRecipe
+      });
+  }
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
+   * @returns {json} updates an existing recipe
+   * @memberof Recipe
+   */
+  updateRecipe(req, res) {
+    const query = req.params.recipeId;
+    if (!query) {
+      return res.status(400)
+        .send('Recipe parameter is needed');
+    }
+    for (let i = 0; i < db.recipes.length; i += 1) {
+      if (parseInt(db.recipes[i].id, 10) === parseInt(req.params.recipeId, 10)) {
+        db.recipes[i].name = req.body.newName || db.recipes[i].name;
+        db.recipes[i].ingredients = [req.body.newIngredients] || db.recipes[i].ingredients;
+        db.recipes[i].description = req.body.newDescription || db.recipes[i].description;
+        return res.status(200)
+          .json({
+            status: 'success',
+            message: 'Recipe modified',
+            recipe: db.recipes[i]
+          });
+      }
+    }
+    return res.status(404)
+      .send(`recipe with id ${query} not found`);
+  }
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
+   * @returns {json} after deleting a particular recipe
+   * @memberof Recipe
+   */
+  deleteRecipe(req, res) {
+    for (let i = 0; i < db.recipes.length; i += 1) {
+      if (parseInt(db.recipes[i].id, 10) === parseInt(req.params.recipeId, 10)) {
+        db.recipes.splice(i, 1);
+        return res.status(200)
+          .json({
+            status: 'success',
+            message: 'Recipe has been deleted'
+          });
+      }
+    }
+    return res.status(404)
+      .send('Recipe not found');
+  }
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
+   * @returns {json} the result from the api
+   * @memberof Recipe
+   */
+  getAllRecipes(req, res) {
+    if (req.query.sort) {
+      const sorted = db.recipes.sort((a, b) => b.upVote - a.upVote);
+      return res.status(200)
+        .json({
+          status: 'success',
+          data: sorted
+        });
+    }
+    return res.status(200)
+      .json({
+        status: 'success',
+        recipes: db.recipes
+      });
+  }
+}
+
+export default Recipe;
